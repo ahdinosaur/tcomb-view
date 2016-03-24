94aea63e9825ce2e9fafb138bb3d8231b26f6c49
Michael Williams <michael.williams@enspiral.com>
2016-03-25 11:35:43 +1300

    it works!

--- example/index.js
+++ example/index.js
@@ -1,9 +1,23 @@
-var ReactDOM = require('react-dom')
-var h = require('r-dom')
+var t = require('tcomb')
+var yo = require('yo-yo')
+var View = require('../')
 
-var PixelPicker = require('../')
+var Thing = t.struct({
+  name: t.String
+})
 
-ReactDOM.render(
-  h(PixelPicker, {}),
-  document.querySelector('main')
-)
+var view = View({
+  type: Thing,
+  update: function (value) {
+    console.log('value', value)
+    yo.update(main, view(value))
+  },
+  hx: yo
+})
+
+var thing = Thing({
+  name: 'computer'
+})
+
+var main = document.querySelector('main')
+yo.update(main, view(thing))
--- index.js
+++ index.js
@@ -1,67 +1,91 @@
-var React = require('react')
-var h = require('r-dom')
-var t = require('tcomb-form')
-var ColorType = require('t-color')
+var t = require('tcomb')
+var map = require('lodash/map')
 
-module.exports = React.createClass({
-  displayName: "PixelPicker",
+module.exports = View
 
-  getInitialState () {
-    return {
-      value: ColorType({
-        type: 'rgb',
-        red: 255,
-        green: 0,
-        blue: 0
-      })
-    }
-  },
+function View (options) {
+  options = options || {}
 
-  onSubmit (evt) {
-    evt.preventDefault()
-    var value = this.refs.form.getValue()
-    this.setState({ value: ColorType(value) })
-  },
-
-  onChange (value) {
-    const prevValue = this.state.value
-    if (value.type !== prevValue.type) {
-      const converted = prevValue.convert(value.type)
-      this.setState({ value: converted })
-    }
-  },
-
-  componentDidMount () {
-    this.styleColor(this.state.value)
-  },
-
-  componentDidUpdate (props, state) {
-    this.styleColor(this.state.value)
-  },
-
-  render() {
-    return h('form', { onSubmit: this.onSubmit }, [
-      h(t.form.Form, {
-        ref: 'form',
-        type: ColorType,
-        value: this.state.value,
-        onChange: this.onChange
-      }),
-      h('div', { className: 'form-group' }, [
-        h('button', { type: 'submit', className: 'btn btn-primary' }, 'Save')
-      ])
-    ])
-  },
-
-  styleColor (color) {
-    const rgb = color.convert('rgb')
-    const colorString = [
-      'rgb(',
-      Math.round(rgb.red) + ',',
-      Math.round(rgb.green) + ',',
-      Math.round(rgb.blue),
-      ')'
-    ].join('')
-    document.body.style.backgroundColor = colorString
+  var type = options.type
+  if (!t.isType(type)) {
+    throw new Error('tcomb-view: `options.type` is a required tcomb type.')
   }
-})
+
+  var kind = type.meta.kind
+  var view = type.view || (t[kind] != null ? t[kind].view : null)
+  if (view == null) {
+    throw new Error('tcomb-view: cannot find view from type or kind.')
+  }
+
+  var layout = options.layout || defaultLayout
+
+  return layout(view(options))
+}
+
+function defaultLayout (view) {
+  return view
+}
+
+t.String.view = function viewString (options) {
+  return function (value) {
+    return options.hx`
+      <input type='text'
+        value=${value}
+        oninput=${onInput}
+      />
+    `
+
+    function onInput (evt) {
+      options.update(evt.target.value)
+    }
+  }
+}
+
+t.irreducible.view = function irreducibleView (options) {
+  return function (value) {
+    return options.hx`
+      <div className='value'>
+        ${value}
+      </div>
+    `
+  }
+}
+
+t.struct.view = function structView (options) {
+  return function (props) {
+    return options.hx`
+      <div className='props'>
+        ${map(options.type.meta.props, (type, key) => {
+          var viewOptions = Object.assign({}, options, {
+            type, update: updateFor(key)
+          })
+          var view = View(viewOptions)
+          var value = props[key]
+
+          return options.hx`
+            <div className='key-value-pair'>
+              <div className='key'>
+                ${key}
+              </div>
+              <div className='value'>
+                ${view(value) }
+              </div>
+            </div>
+          `
+        })}
+      </div>
+    `
+
+    function updateFor (key) {
+      return function (value) {
+        console.log('key', key, 'value', value, props)
+        var nextValue = options.type.update(props, {
+          [key]: {
+            $set: value
+          }
+        })
+        options.update(nextValue)
+      }
+    }
+  }
+}
